91f76baad211e290a28cc0d92747f46e20d6b9bb
golazo89 <149277163+golazo89@users.noreply.github.com>
2025-01-13 09:20:25 +0700

    first init github

---
+++ config/rajaongkir.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_key' => env('RAJAONGKIR_API_KEY', '650d59ab0111409f4492d5f4e821f683'),
+    'base_url' => env('RAJAONGKIR_BASE_URL', 'https://api.rajaongkir.com/starter'),
+    'origin' => env('RAJAONGKIR_ORIGIN', null),
+];
